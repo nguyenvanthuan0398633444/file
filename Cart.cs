c0050e033c118bb3ace048edfec69a4f80471fc7
@nvThuan <53548696+nguyenvanthuan0398633444@users.noreply.github.com>
2021-03-15 16:33:43 +0700

    Add files via upload

---
+++ Cart.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopWebApplication.Models;
+
+namespace ShopWebApplication.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int count { get; set; }
+        public Nullable<decimal> cost { get; set; }
+        public string photo { get; set; }
+        public Nullable<decimal> intoMoney { get; set; }
+        public Cart(int Ids, int counts)
+        {
+            using (ShopEntityDb db = new ShopEntityDb())
+            {
+                this.Id = Ids;
+                Product sp = db.Products.Single(n => n.ProductID == Ids);
+                name = sp.ProductName;
+                cost = sp.Price;
+                photo = sp.PhotoPath;
+                count = counts;
+                intoMoney = cost * count;
+            }
+                
+        }
+        public Cart(int Ids)
+        {
+            using (ShopEntityDb db = new ShopEntityDb())
+            {
+                this.Id = Ids;
+                Product sp = db.Products.Single(n => n.ProductID == Ids);
+                name = sp.ProductName;
+                cost = sp.Price;
+                photo = sp.PhotoPath;
+                count = 1;
+                intoMoney = cost * count;
+            }
+
+        }
+        public Cart()
+        {
+
+        }
+    }
+}
---
+++ CartController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopWebApplication.Models;
+
+namespace ShopWebApplication.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        ShopEntityDb db = new ShopEntityDb();
+       
+        public List<Cart> getCart()
+        {
+            List<Cart> lsCart = Session["Cart"] as List<Cart>;
+            if(lsCart == null)
+            {
+                List<Cart> ls = new List<Cart>();
+                Session["Cart"] = lsCart;
+                return lsCart;
+            }
+            return lsCart;
+        }
+        public ActionResult AddCart( int Ids, string url)
+        {
+            Product sp = db.Products.SingleOrDefault(n => n.ProductID == Ids);
+            if(sp == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            //lay gio hang
+            List<Cart> lsCart = new List<Cart>();
+            lsCart = getCart();
+            //san pham ton tai trong gio hang
+            if (lsCart != null)
+            {
+                Cart spChesk = lsCart.SingleOrDefault(n => n.Id == Ids);
+                if (spChesk != null)
+                {
+                    spChesk.count++;
+                    return Redirect(url);
+                }
+            }
+
+            Cart itemCart = new Cart(Ids);
+            lsCart.Add(itemCart);
+            return Redirect(url);            
+        }
+        public double sumProduct()
+        {
+            List<Cart> lsCart = Session["Cart"] as List<Cart>;
+            if(lsCart == null)
+            {
+                return 0;
+            }
+            return lsCart.Sum( n => n.count) ;
+
+        }
+        public Nullable<decimal> sumMoney()
+        {
+            List<Cart> lsCart = Session["Cart"] as List<Cart>;
+            if (lsCart == null)
+            {
+                return 0;
+            }
+            return lsCart.Sum(n => n.intoMoney);
+
+        }
+        public ActionResult CartPatial()
+        {
+            if(sumProduct() == 0)
+            {
+                ViewBag.sumProduct = 0;
+                return PartialView();
+            }
+            ViewBag.sumProduct = sumProduct();
+            return PartialView();
+        }
+        public ActionResult Index()
+        {
+            List<Cart> lstCart = getCart();
+            return View(lstCart);
+        }
+
+    }
+}
