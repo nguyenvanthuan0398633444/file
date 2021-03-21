23301b9873207297c66c6ea4ac41b42c648f86dd
@nvThuan <53548696+nguyenvanthuan0398633444@users.noreply.github.com>
2021-03-21 22:51:14 +0700

    Add files via upload

---
+++ ICategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList;
+using ShopWebApplication.Models;
+
+namespace ShopWebApplication.Services.Interfaces
+{
+    interface ICategory
+    {
+        IPagedList<Category> GetListCategory(int? page, string search);
+        Category NewCategory(Category category);
+        Category EditCategory(Category model);
+        Boolean DeleteCategory(int id);
+    }
+}
